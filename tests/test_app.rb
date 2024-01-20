40f7931b14d9e83fdce9cdae4f01d80150a72fb6
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-01-19 18:53:23 -0600

    MiniTest

--- tests/test_app.rb
+++ tests/test_app.rb
@@ -1,4 +1,14 @@
 require 'minitest/autorun'
-require './game'
+require './game_ref'
 
 
+class TestGame < Minitest::Test
+    def test_game_rules
+        game = Game.new
+        assert_equal puts, game.rules, "Game rules method failed"
+    end
+    def test_game_over
+        game = Game.new
+        assert_equal puts, game.game_over, "Game over method failed"
+    end
+end
